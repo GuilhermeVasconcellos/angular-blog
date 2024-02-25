ab777a89a58603a0486d4a4b1fb16a4d6875788f
Guilherme Vasconcellos <gt.vasconcellos@gmail.com>
2024-02-25 18:08:46 -0300

    new descriptions added

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -3,24 +3,24 @@ export const dataFake = [
         "id": 1,
         "photo": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=1200&aspectRatio=1.78&format=webp",
         "title": "Novo filme do Homem de Ferro anunciado, confira!",
-        "description": "Marvel Studios announces the launching of a new episode of Iron Man"
+        "description": "Marvel Studios announces the launching of a new episode of Iron Man."
     },
     {
         "id": 2,
         "photo": "https://proxy.olhardigital.com.br/wp-content/uploads/2023/12/imagem_2023-12-19_013932059-1024x576.png",
         "title": "Novidades para o próximo ano referente aos heróis da Marvel",
-        "description": "Nova série anunciada no Disney +"
+        "description": "Nova série anunciada no Disney +."
     },
     {
         "id": 3,
         "photo": "https://isabelaboscov.files.wordpress.com/2016/05/x-men_5_feat.jpg?w=1038&h=520&crop=1",
         "title": "Novo filme do X-Men com data de lançamento definida",
-        "description": "ola"
+        "description": "A partir do próximo mês de junho, você poderá desfrutar das novas aventuras do X-Men."
     },
     {
         "id": 4,
         "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmNyeJbwV9Do3qfxZddp5tVThDHBaxdSDKALusl5P3PA&s",
         "title": "Marvel anuncia lançamento de novo filme do Thor para 2025",
-        "description": "ola"
+        "description": "Thor está de volta, com muita ação, neste novo episódio da saga."
     }
 ]
