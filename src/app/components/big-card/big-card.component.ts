e774c7d906fe18a4ce72f2975d4c25c6c9dfc9f4
Guilherme Vasconcellos <gt.vasconcellos@gmail.com>
2024-02-25 16:10:46 -0300

    Components with dynamic input

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   standalone: true,
   imports: [],
   templateUrl: './big-card.component.html',
-  styleUrl: './big-card.component.css'
+  styleUrls: ['./big-card.component.css']
 })
-export class BigCardComponent {
+export class BigCardComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  @Input()
+  mainCardPhoto:string = "";
+  @Input()
+  mainCardTitle:string = "";
+  @Input()
+  mainCardDescription:string = "";
 
 }
